806c7b0b12ac99ec83128bbb63bb5122e26c8701
maria-eduarda-schwinn <maria.eduarda.schwinn@gmail.com>
2024-11-19 18:37:49 -0300

    Add S3 client integration for storing URL data

--- src/main/java/com/rocketseat/createUrlShortener/Main.java
+++ src/main/java/com/rocketseat/createUrlShortener/Main.java
@@ -6,10 +6,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
@@ -25,8 +30,25 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
 
         var originalUrl = bodyMap.get("originalUrl");
         var expirationTime = bodyMap.get("expirationTime");
+        var expirationTimeInSeconds = Long.parseLong(expirationTime);
 
         var shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        var urlData = new UrlData(originalUrl, expirationTimeInSeconds);
+
+        try {
+            var urlDataJson = objectMapper.writeValueAsString(urlData);
+
+            PutObjectRequest request =
+                    PutObjectRequest.builder()
+                            .bucket("url-shortener-lambda-data")
+                            .key(shortUrlCode + ".json")
+                            .build();
+
+            s3Client.putObject(request, RequestBody.fromString(urlDataJson));
+        } catch (Exception e) {
+            throw new RuntimeException("Error saving URL data to S3: " + e.getMessage(), e);
+        }
 
         Map<String, String> response = new HashMap<>();
         response.put("code", shortUrlCode);
